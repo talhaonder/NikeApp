9e5c6b8f4bc7b799aa4d2c5a62db20ec4a2cf06f
talhaonder <ltalhaonderl@gmail.com>
2024-02-15 14:59:31 +0300

    Product Details Screen

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import AppNavigation from './navigation/AppNavigation'
 import { StyleSheet, View, StatusBar, } from 'react-native';
 import ProductsScreen from './src/screens/ProductsScreen';
+import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductsScreen />
+      {/*<ProductsScreen />*/}
+      <ProductDetailsScreen/>
       <StatusBar styles="auto" />
     </View>
   );
@@ -16,7 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
